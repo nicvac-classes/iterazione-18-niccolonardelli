d35c4856a1f64ce4f5b7c3d7e4aa46ed02ebbdc9
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-11-07 09:32:45 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int i, acqua, somma;
+    somma = 0;
+    for (i=1; i<=7; i=i+1)
+    {
+      cout << "giorno " << i << endl;
+      cout << "inserire il livello dell'acqua di oggi" << endl;
+      cin>>acqua;
+      somma = somma + acqua;
+      if (acqua>0)
+      {
+        if (acqua>10)
+        {
+            cout <<"acqua alta" << endl;
+            cout << "nel giorno " << i << " l'acqua supera il livello di 10 cm" << endl; 
+        }
+        else 
+        {
+            cout << "acqua alta" << endl;
+        }
+      }
+      else
+      {
+        cout << "livello normale" << endl; 
+      }
+      
+
+    }
+    cout << "la media del livello dell'acqua settimanale è di " << somma/7 << endl;
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
